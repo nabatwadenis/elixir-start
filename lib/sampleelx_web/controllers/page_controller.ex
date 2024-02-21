a1f6d32e15247fcd66cd880cba97fbdcde9792bc
denisnabatwa <dnabatwa9@gmail.com>
2024-02-21 14:17:46 +0300

    adding new page

--- lib/sampleelx_web/controllers/page_controller.ex
+++ lib/sampleelx_web/controllers/page_controller.ex
@@ -6,4 +6,15 @@ defmodule SampleelxWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    IO.puts("Users names and emails!")
+    users =[
+      %{id: 1, name: "Alice", email: "alice@email.com"},
+      %{id: 2, name: "grace", email: "grace@email.com"},
+
+    ]
+    #render(conn, :users, users: users, layout: false)
+    json(conn, %{users: users})
+  end
 end
